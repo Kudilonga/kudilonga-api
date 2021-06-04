d80d132e63abd6ca9481f8c3785cf2b0c1f3b2b6
gentildpinto <gentil.junior.gp@gmail.com>
2021-06-04 22:22:08 +0100

    added Language Unit test file

---
+++ tests/Unit/LanguageTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\Language;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+test('language', function () {
+    $this->assertTrue(true);
+});
