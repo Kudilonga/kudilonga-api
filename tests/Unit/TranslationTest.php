bd17960c3ef08aa6fc5b2b3a60ea00869d2d6511
gentildpinto <gentil.junior.gp@gmail.com>
2021-06-04 23:17:06 +0100

    created translation unit test file

---
+++ tests/Unit/TranslationTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\Translation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+test('translation', function () {
+    $this->assertTrue(true);
+});
