120b4d18466d24e136f36517f21193faf95f7b8c
gentildpinto <gentil.junior.gp@gmail.com>
2021-06-04 22:41:21 +0100

    created Word unit test file

---
+++ tests/Unit/WordTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\Language;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+test('word', function () {
+    $this->assertTrue(true);
+});
