7e3458efa5c819661f9de9453e6e9291f1c2d851
gentildpinto <gentil.junior.gp@gmail.com>
2021-06-05 16:58:35 +0100

    import HttpResponses on app controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Traits\HttpResponses;
+use Illuminate\Foundation\{
+    Bus\DispatchesJobs,
+    Validation\ValidatesRequests,
+    Auth\Access\AuthorizesRequests
+};
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, HttpResponses;
 }
